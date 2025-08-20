f7009bdd34bd244592cc27db19070ccc51d7de65
진돌 <wlstjrghdud@naver.com>
2025-08-21 01:54:14 +0900

    테스트 케이스에 알림 기능 추가 및 이벤트 날짜 수정
    - 이벤트 날짜를 '2025-10-01'로 수정하여 일관성 유지
    - useNotifications 훅에 대한 테스트 케이스 추가: 초기 상태, 알림 생성, 알림 제거, 중복 알림 방지 검증

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -1,14 +1,69 @@
 import { act, renderHook } from '@testing-library/react';
 
+import { events } from '../../__mocks__/response/events.json' assert { type: 'json' };
 import { useNotifications } from '../../hooks/useNotifications.ts';
 import { Event } from '../../types.ts';
-import { formatDate } from '../../utils/dateUtils.ts';
-import { parseHM } from '../utils.ts';
+import { setupMockDate } from '../utils.ts';
 
-it('초기 상태에서는 알림이 없어야 한다', () => {});
+setupMockDate(new Date('2025-10-01T08:50:00'));
 
-it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+it('초기 상태에서는 알림이 없어야 한다', () => {
+  const { result } = renderHook(() => useNotifications(events as Event[]));
 
-it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
+  expect(result.current.notifications).toEqual([]);
+});
 
-it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', async () => {
+  const { result } = renderHook(() => useNotifications(events as Event[]));
+
+  await act(async () => {
+    await vi.advanceTimersByTimeAsync(1000);
+  });
+
+  expect(result.current.notifications).toEqual([
+    {
+      id: '1',
+      message: '10분 후 기존 회의 일정이 시작됩니다.',
+    },
+  ]);
+});
+
+it('index를 기준으로 알림을 적절하게 제거할 수 있다', async () => {
+  const { result } = renderHook(() => useNotifications(events as Event[]));
+
+  await act(async () => {
+    await vi.advanceTimersByTimeAsync(1000);
+  });
+
+  act(() => {
+    result.current.removeNotification(0);
+  });
+
+  expect(result.current.notifications).toEqual([]);
+});
+
+it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', async () => {
+  const { result } = renderHook(() => useNotifications(events as Event[]));
+
+  await act(async () => {
+    await vi.advanceTimersByTimeAsync(1000);
+  });
+
+  expect(result.current.notifications).toEqual([
+    {
+      id: '1',
+      message: '10분 후 기존 회의 일정이 시작됩니다.',
+    },
+  ]);
+
+  await act(async () => {
+    await vi.advanceTimersByTimeAsync(1000);
+  });
+
+  expect(result.current.notifications).toEqual([
+    {
+      id: '1',
+      message: '10분 후 기존 회의 일정이 시작됩니다.',
+    },
+  ]);
+});
